1ebf72a5913a747597b4af00ad97a2473cb81e88
hikiray <satalomaksimka12345678910@gmail.com>
2025-06-12 08:11:04 +0700

    repair urls

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('api/', include('api.urls')),
-    path('', include('recipes.urls')),
-    path('', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
